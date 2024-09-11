d3fede0ad9d799ffe9d6950f03f6ae2f30e02b46
LucasGitHubx <lucasdarosa450@gmail.com>
2024-09-11 18:31:30 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./app.css";
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="">¿Qué ofrecemos?</a>
+            </li>
+            <li>
+              <a href="">Opiniones</a>
+            </li>
+
+            <li>
+              <a href="">¿Por qué nosotros?</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
